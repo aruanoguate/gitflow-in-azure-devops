ace67da9d7d99ad2c3ea44edc840ceb963cdd25d
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-15 19:39:07 -0600

    Adding ECHO OFF

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+ECHO OFF
 REM This should iterate over all the files in the 'bat' directory
 REM and will execute them to add all the git aliases
 for /f "usebackq delims=|" %%f in (`dir /b "bat"`) do call "bat/%%f"
