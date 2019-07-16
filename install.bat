d2d971902c1d47886f473eb57dde8bbe9ff3f842
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-15 22:10:26 -0600

    General structure modification

--- install.bat
+++ install.bat
@@ -1,6 +1,16 @@
 ECHO ON
-REM This should iterate over all the modules and execute
-REM the init script on each one
-FOR /f %%G in ('dir /b modules') DO (
-    CALL "modules/%%G/init.bat"
+FOR /f %%M IN ('dir /b modules') DO (
+    FOR /f %%S IN ('dir /b modules\%%M\*.sh') DO (
+        CALL :PROCESS_SHELL %%M %%S
+    )
 )
+GOTO :eof
+
+:PROCESS_SHELL
+SET aliasName=%1.%2
+SET aliasName=%aliasName:.sh=%
+SET filename='%CD%\modules\%1\%2'
+SET aliasCmd="!func() { sh %filename%; }; func"
+SET gitCmd=git config --global alias.%aliasName% %aliasCmd%
+REM %gitCmd%
+EXIT /B
---
+++ modules/general/unstage.sh
@@ -0,0 +1,2 @@
+# To unstage all files
+git config --global alias.unstage "reset HEAD --"
