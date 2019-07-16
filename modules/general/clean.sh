ed87ef78e69fbed44d6c5f74610d4459eeee64de
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-16 15:33:29 -0600

    Include prune in clean script

--- modules/general/clean.sh
+++ modules/general/clean.sh
@@ -2,8 +2,10 @@
 
 # To remove all branches already merged with the exception of master and develop
 git fetch --all
+git remote prune origin
 git checkout master
 git reset --hard origin/master
 git checkout develop
 git reset --hard origin/develop
+git branch --merged | grep -v "\*" | grep -Ev "(\*|master|develop)" | xargs -n 1 git push origin --delete
 git branch --merged | grep -v "\*" | grep -Ev "(\*|master|develop)" | xargs -n 1 git branch -d
--- modules/general/prune.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Similar to clean, but on the remote
-git remote prune origin
