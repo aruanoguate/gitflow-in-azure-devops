e448c289ab397c4cfa15cc64f1ae35b5f6fa0872
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-16 15:10:42 -0600

    Fetch before do cleanup

--- modules/general/clean.sh
+++ modules/general/clean.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # To remove all branches already merged with the exception of master and develop
+git fetch --all
+git checkout master
+git reset --hard origin/master
+git checkout develop
+git reset --hard origin/develop
 git branch --merged | grep -v "\*" | grep -Ev "(\*|master|develop)" | xargs -n 1 git branch -d
