3ffb4cf88635bae54ed1da6dafa8930938999fe5
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-16 15:33:41 -0600

    New handling for feature branches

---
+++ modules/flow/feature.publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$BRANCH" != "feature"* ]]; then
+  echo 'This can only be executed on feature branches';
+  exit 1;
+fi
+
+git fetch --all
+git checkout develop
+git reset --hard origin/develop
+git checkout $BRANCH
+git rebase develop
+git push origin $BRANCH
--- modules/flow/feature.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git checkout develop
-git pull origin develop
-git branch feature-$1 develop
-git checkout feature-$1
-git push -u origin feature-$1
