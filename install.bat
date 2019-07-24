de615432af3ac19303016c9290c4666aa6d70175
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-23 19:50:54 -0600

    Rename from "modules" to "aliases"

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 ECHO OFF
-FOR /f %%M IN ('dir /b modules') DO (
-    FOR /f %%S IN ('dir /b modules\%%M\*.sh') DO (
-        CALL :PROCESS_SHELL %%M %%S
+FOR /f %%A IN ('dir /b aliases') DO (
+    FOR /f %%S IN ('dir /b aliases\%%A\*.sh') DO (
+        CALL :PROCESS_SHELL %%A %%S
     )
 )
 GOTO :eof
@@ -9,7 +9,7 @@ GOTO :eof
 :PROCESS_SHELL
     SET aliasName=%1.%2
     SET aliasName=%aliasName:.sh=%
-    SET filename='%CD%\modules\%1\%2'
+    SET filename='%CD%\aliases\%1\%2'
     SET aliasCmd="!func() { sh %filename% $@; }; func"
     SET gitCmd=git config --global alias.%aliasName% %aliasCmd%
     %gitCmd%
