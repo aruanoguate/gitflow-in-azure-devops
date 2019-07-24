c92add475f401b232718644e19542f50d1897b79
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-23 20:50:47 -0600

    Upgrade installation process

--- install.bat
+++ install.bat
@@ -1,9 +1,14 @@
 ECHO OFF
+mkdir %userprofile%\gitflow\
+xcopy /E /Y modules %userprofile%\gitflow\modules\
+xcopy /E /Y aliases %userprofile%\gitflow\aliases\
+pushd %userprofile%\gitflow\
 FOR /f %%A IN ('dir /b aliases') DO (
     FOR /f %%S IN ('dir /b aliases\%%A\*.sh') DO (
         CALL :PROCESS_SHELL %%A %%S
     )
 )
+popd
 GOTO :eof
 
 :PROCESS_SHELL
