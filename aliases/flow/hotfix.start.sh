69f9fb79deb9da1ea181b2db235bd9943b8b402b
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-23 22:20:29 -0600

    New logic to create hotfixes

---
+++ aliases/flow/hotfix.start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Parameters 
+BRANCH=$1;
+
+# Imports
+. $HOME/gitflow/modules/flow.sh;
+
+# Validations
+verifyBranchNameProvided $BRANCH;
+
+# Process
+forceBranchUpdateFromOrigin "master";
+git branch hotfix/$BRANCH master;
+git checkout hotfix/$BRANCH;
+git push --set-upstream origin hotfix/$BRANCH;
