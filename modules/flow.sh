057d32ed13706394893eb05e0c019a0644fb060b
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-23 21:39:34 -0600

    Format changes

--- modules/flow.sh
+++ modules/flow.sh
@@ -7,36 +7,45 @@ function showWarning() {
 }
 
 function updateDevelopBranch() {
-    local BRANCH="$(getBranchName)"
+    local BRANCH="$(getBranchName)";
     git fetch origin develop;
     git checkout develop;
     git reset --hard origin/develop;
-    git checkout $BRANCH
+    git checkout $BRANCH;
 }
 
 function getBranchName() {
     local BRANCH=$(git rev-parse --abbrev-ref HEAD);
-    echo "$BRANCH"
+    echo "$BRANCH";
 }
 
 function verifyBranchType() {
     local BRANCHTYPE=$1;
-    local BRANCH="$(getBranchName)"
+    local BRANCH="$(getBranchName)";
     
-    if [[ "$BRANCH" != "$BRANCHTYPE/"* ]]; then
-        showError "This can only be executed on $BRANCHTYPE branches"
-        exit 1
+    if [[ "$BRANCH" != "$BRANCHTYPE/"* ]]; 
+    then
+        showError "This can only be executed on $BRANCHTYPE branches";
+        exit 1;
     fi
 }
 
 function verifyUpToDateBranch() {
-    local BRANCH="$(getBranchName)"
+    local BRANCH="$(getBranchName)";
 
-    git fetch origin $BRANCH
+    git fetch origin $BRANCH;
 
-    local LAST_LOCAL_COMMIT=$(git rev-parse HEAD)
-    local LAST_UPSTREAM_COMMIT=$(git rev-parse @{u})
+    local LAST_LOCAL_COMMIT=$(git rev-parse HEAD);
+    local LAST_UPSTREAM_COMMIT=$(git rev-parse @{u});
     
+    if [[ "$LAST_LOCAL_COMMIT" != "$LAST_UPSTREAM_COMMIT" ]]; 
+    then
+        showError "The branch should be up to date with its upstream";
+        git status;
+        exit 1;
+    fi
+}
+
     if [[ "$LAST_LOCAL_COMMIT" != "$LAST_UPSTREAM_COMMIT" ]]; then
         showError "The branch should be up to date with its upstream"
         git status
