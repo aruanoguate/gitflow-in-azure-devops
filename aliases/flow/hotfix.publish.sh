a3f98ae5022c4801e5d0849d3a061825dca38bd3
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-07-29 19:03:09 -0600

    Delete source branch flag enabled

--- aliases/flow/hotfix.publish.sh
+++ aliases/flow/hotfix.publish.sh
@@ -13,12 +13,14 @@ tryRebase "master";
 git push --force;
 HOTFIX_NAME="$(getBranchNameWithoutPrefix)";
 git pr create \
+  --delete-source-branch \
   --open \
   --output table \
   --reviewers "CompIQ Team" \
   --target-branch master \
   --title "Hotfix completed: $HOTFIX_NAME";
 git pr create \
+  --delete-source-branch \
   --output table \
   --reviewers "CompIQ Team" \
   --target-branch develop \
