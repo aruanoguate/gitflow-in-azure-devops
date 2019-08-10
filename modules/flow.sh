44f92f549bd05799590eb4636708458c522732dc
Alvaro Enrique Ruano <alvaro.ruano@conduent.com>
2019-08-09 19:59:40 -0600

    Adding tryCreateBranch function

--- modules/flow.sh
+++ modules/flow.sh
@@ -143,3 +143,19 @@ function tryRebase()
         exit 1;
     fi
 }
+
+function tryCreateBranch()
+{
+    local BRANCH_TO_CREATE=$1;
+    local BRANCH_TO_COPY=$2;
+
+    git rev-parse --verify $BRANCH_TO_CREATE;
+    local BRANCH_VERIFICATION_RESULT=$?;
+
+    if [[ $BRANCH_VERIFICATION_RESULT -ne 0 ]];
+    then
+        git branch $BRANCH_TO_CREATE $BRANCH_TO_COPY;
+    else
+        showWarning "Branch '$BRANCH_TO_CREATE' already exists, it was not modified";
+    fi
+}
