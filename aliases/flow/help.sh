df946f21b7531d5229c72a75898d28c5858186e0
Ruano ixcaragua, Alvaro E <Alvaro.Ruano@conduent.com>
2020-01-24 21:26:44 -0600

    Disable md.nanorc which is not working, to revisit later

--- aliases/flow/help.sh
+++ aliases/flow/help.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Imports
+. $HOME/gitflow/modules/show.sh
+
 # Process
-cp $HOME/gitflow/config/md.nanorc /usr/share/nano/
+# cp $HOME/gitflow/config/md.nanorc /usr/share/nano/
 nano -v -x -m $HOME/gitflow/HELP.md
 showSuccess "The user manual was shown";
