232318abfe3b6aae0203ce21511b0373cbce6667
Ruano ixcaragua, Alvaro E <Alvaro.Ruano@conduent.com>
2020-01-24 20:17:36 -0600

    This should be executed from master

--- aliases/general/clean.sh
+++ aliases/general/clean.sh
@@ -6,12 +6,12 @@
 
 # Validations
 verifyInGitRepo;
+verifyBranchNameIs "master";
 verifyNoUncommitedChanges;
 
 # To remove all branches already merged with the exception of master and develop
 git fetch --all
 git remote prune origin
-git checkout master
 git reset --hard origin/master
 git checkout develop
 git reset --hard origin/develop
